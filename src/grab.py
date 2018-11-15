10f7c74f6bc11ff51d10a52ce3b2bda8e91cf05e
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2018-11-15 14:03:13 +1300

    plots working for monthly ice area average

--- src/grab.py
+++ src/grab.py
@@ -6,9 +6,16 @@ def get(path,modelname):
 	"""imports variables from NetCDF files with specified path and variable name"""
 	os.chdir("../../../../")
 	os.chdir("{}/{}/{}".format(path,modelname,"ice"))
-	alldata = []
+	filecount=0
+	#simply counting number of files in directory.
 	for filename in os.listdir('./'):
-		testdata = Dataset(filename)
-		alldata.append(testdata)
-		print "storing {}".format(filename)
+		filecount +=1
+	#now we will sort the files based on month.
+	alldata=[[],[],[],[],[],[],[],[],[],[],[],[]]
+	for filename in os.listdir('./'):
+		testdata = Dataset(filename) #grabbing dataset from file
+		monthstr = filename[19:21] #grabbing month value from filename... format does not vary
+		month = int(monthstr)-1 #chucking dataset into alldata's "month" boxes.. alldata[0] is jan, alldata [1] is feb ect.
+		print monthstr
+		alldata[month].append(testdata)
 	return alldata
--- src/plot.py
+++ src/plot.py
@@ -3,6 +3,8 @@ import grab
 import mystats
 from netCDF4 import *
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
 	"""grabbing dataset for model au866"""
@@ -13,10 +15,25 @@ def seasonal_ice_area(model_data):
 	"""calculates the mean seasonal ice area for a model. Returns a 1x12 vector
 	the total ice area is calculate by aice*tarea, where aice is the proportion that ice fills up the gridbox (goes from 0-1)
 	and t-area is the area of each gridbox."""
-	for run in model_data:
-		aice = run.variables['aice']
-		tarea = run.variables['tarea']
-		print aice, tarea
 	
+	month_mean = np.zeros(12) # a vector that will be populated with the mean ice area per month...	
+	
+	#Going through each entry in each month. Finding the mean total area for each month.
+	for i, month in enumerate(model_data):	
+		run_num = len(month) #number of runs per month
+		total_area = 0
+		for run in month:
+			tarea = np.ma.array(run.variables['tarea'][:,:],dtype='float64')
+			aice = np.ma.squeeze(np.ma.array(run.variables['aice'][:,:],dtype='float64'))
+			total_area += np.ma.sum(aice*tarea)
+		month_mean[i] = (total_area/run_num)
+	
+	#now plotting 
+	plt.plot(month_mean)
+	plt.show()
+	plt.ylabel('Ice area (m^2)')
+	plt.xlabel('Months of the year')
+	plt.title('Seasonal')
+
 if __name__=='__main__':
 	main()
